7191d6d2cc7665368723c45bf06cb9a25c3000f5
ZileidyAB <zileidy.arguello@upa.edu.py>
2020-11-10 21:41:59 -0300

    Intento de registro de ejercicios

--- misgruposv01/src/main/java/py/com/misgruposv01/activities/RegistrarEjercicioActivity.java
+++ misgruposv01/src/main/java/py/com/misgruposv01/activities/RegistrarEjercicioActivity.java
@@ -1,11 +1,15 @@
 package py.com.misgruposv01.activities;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import py.com.misgruposv01.R;
